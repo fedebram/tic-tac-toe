832955a1b1fd5882cfa24088973a6797966bcf78
fedebram <163430291+fedebram@users.noreply.github.com>
2024-04-24 15:03:48 +0200

    Project done with shitty code

--- javascrpit.js
+++ javascrpit.js
@@ -1,72 +1,113 @@
 const gameBoard = [];
+const game = document.querySelector(".game-board");
 
-function Cell (value, check, row, column) {
+function Cell(value, check, row, column) {
     this.value = value;
     this.check = check;
 }
 let row = 3;
 let column = 3;
+let playerCount = 1;
 
 for (let i = 0; i < row; i++) {
     gameBoard[i] = [];
     for (let j = 0; j < column; j++) {
         gameBoard[i][j] = new Cell("", false);
+        gameBoard[i][j] = document.createElement("div");
+        game.appendChild(gameBoard[i][j]);
+        gameBoard[i][j].id = `cell${i}-${j}`;
+        gameBoard[i][j].addEventListener("click", playGame);
     }
 }
 
-function checkRow () {
-count = 0;
-for (let i = 0; i < row; i++) {
-    for (let j = 0; j < column; j++) {
-        if (gameBoard[i][j].value === "X") count ++;
-        if (count !== 3) break;
+function checkRow() {
+    let countX = 0;
+    let countO = 0;
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            if (gameBoard[i][j].value === "X") countX++;
+            else if (gameBoard[i][j].value === "O") countO++;
+            if (countX === 3) console.log("X win");
+            if (countO === 3) console.log("O win");
+        }
+        countX = 0;
+        countO = 0;
     }
 }
-}
 
-function checkColumn () {
-    count = 0;
+function checkColumn() {
+    let countX = 0;
+    let countO = 0;
     for (let j = 0; j < column; j++) {
         for (let i = 0; i < row; i++) {
-            if (gameBoard[i][j].value === "X") count ++;
-            if (count !== 3) break;
+            if (gameBoard[i][j].value === "X") countX++;
+            else if (gameBoard[i][j].value === "O") countO++;
+            if (countX === 3) console.log("X win");
+            if (countO === 3) console.log("O win");
         }
+        countX = 0;
+        countO = 0;
     }
 }
 
-function checkDiagonals () {
-    count = 0;
-    if ((gameBoard[0][0].value === "X") && (gameBoard[1][1].value === "X") && (gameBoard[2][2].value === "X")) ;
-    //win
-    if ((gameBoard[0][2].value === "X") && (gameBoard[1][1].value === "X") && (gameBoard[2][0].value === "X"));
-    //win
+
+function checkDiagonals() {
+    if ((gameBoard[0][0].value === "X") && (gameBoard[1][1].value === "X") && (gameBoard[2][2].value === "X")) console.log("X win");
+    if ((gameBoard[0][0].value === "O") && (gameBoard[1][1].value === "O") && (gameBoard[2][2].value === "O")) console.log("O win");
+
+    if ((gameBoard[0][2].value === "X") && (gameBoard[1][1].value === "X") && (gameBoard[2][0].value === "X")) console.log("X win");
+    if ((gameBoard[0][2].value === "O") && (gameBoard[1][1].value === "O") && (gameBoard[2][0].value === "O")) console.log("O win");
 }
 
+function playGame() {
+    if ((playerCount % 2) !== 0) {
+        if (player1 === "X") {
+            this.value = "X";
+            this.textContent = "X";
+        }
+        else if (player1 === "O") {
+            this.value = "O";
+            this.textContent = "O";
+        }
+    }
+    else if ((playerCount % 2) === 0) {
+        if (player2 === "X") {
+            this.value = "X";
+            this.textContent = "X";
+        }
+        else if (player2 === "O") {
+            this.value = "O";
+            this.textContent = "O";
+        }
+    }
+    checkRow();
+    checkDiagonals();
+    checkColumn();
+    playerCount++;
+}
+
+let buttonX = document.getElementById("X");
+let buttonO = document.getElementById("O");
+//function playerChoice () {
+buttonX.addEventListener("click", () => {
+    player1 = "X";
+    player2 = "O";
+    buttonX.disabled = true;
+    buttonO.disabled = true;
+});
+
+buttonO.addEventListener("click", () => {
+    player1 = "O";
+    player2 = "X";
+    buttonX.disabled = true;
+    buttonO.disabled = true;
+});
 
 
 
-const player1 = "x";
-const player2 = "o";
+let player1 = "";
+let player2 = "";
 
-
-/*function playGame () {
-    //if (check) return;
-    //eventlistener cell
-    gameBoard[i].value = "x";
-    gameBoard[i].check = true;
-    //eventlistener cell
-    gameBoard[i].value = "o";
-    gameBoard[i].check = true;
-    //eventlistner cell
-    gameBoard[i].value = "x";
-    gameBoard[i].check = true;
-    //eventli
-    gameBoard[i].value = "o";
-    gameBoard[i].check = true;
-    //
-    gameBoard[i].value = "x";
-    gameBoard[i].check = true;
-}*/
 
 
 
